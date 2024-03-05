94bc35505c6d36b9d6d0254060348c724668aa99
error23 <error23.d@gmail.com>
2024-03-05 02:22:56 +0100

    * first version of plugin and its extension

--- poetry/src/main/kotlin/com/crow/gradle/plugins/poetry/PoetryPlugin.kt
+++ poetry/src/main/kotlin/com/crow/gradle/plugins/poetry/PoetryPlugin.kt
@@ -1,15 +1,145 @@
 package com.crow.gradle.plugins.poetry
 
+import com.crow.gradle.plugins.poetry.tasks.PoetryConfigInitTask
+import com.crow.gradle.plugins.poetry.tasks.PoetryInitProjectStructureTask
+import com.crow.gradle.plugins.poetry.tasks.PoetryInitPyProjectTask
+import com.crow.gradle.plugins.poetry.tasks.PoetryInitReadMeTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.register
 
-class GetTextPlugin : Plugin<Project> {
+/**
+ * Poetry plugin.
+ * Build and manage Python projects with poetry.
+ */
+class PoetryPlugin : Plugin<Project> {
 
-	private val taskGroup = "i18n"
+	private val taskGroup = "python"
 
 	override fun apply(project: Project) {
-		println("GetTextPlugin apply")
 
+		// Apply dependency plugins
+		project.plugins.apply("base")
+		//project.plugins.apply("idea")
+
+		// Create linux packaging extension
+		val extension = project.extensions.create<PoetryExtension>("poetryConfig")
+		setupExtension(extension)
+
+		// Register tasks
+		project.tasks.register<PoetryConfigInitTask>("poetryConfigInit") {
+			group = taskGroup
+			description = extension.poetryConfigInitTask.description.get()
+			poetryCmd.set(extension.poetryConfigInitTask.poetryCmd)
+			poetryTomlFile.set(extension.poetryConfigInitTask.poetryTomlFile)
+		}
+
+		val poetryInitProjectStructure = project.tasks.register<PoetryInitProjectStructureTask>("poetryInitProjectStructure") {
+			group = taskGroup
+			description = extension.poetryInitProjectStructureTask.description.get()
+			poetryCmd.set(extension.poetryInitProjectStructureTask.poetryCmd)
+			mainSourcesDirectory.set(extension.poetryInitProjectStructureTask.mainSourcesDirectory)
+			mainResourcesDirectory.set(extension.poetryInitProjectStructureTask.mainResourcesDirectory)
+			testSourcesDirectory.set(extension.poetryInitProjectStructureTask.testSourcesDirectory)
+			testResourcesDirectory.set(extension.poetryInitProjectStructureTask.testResourcesDirectory)
+		}
+
+		val poetryInitReadMe = project.tasks.register<PoetryInitReadMeTask>("poetryInitReadMe") {
+			group = taskGroup
+			description = extension.poetryInitReadMeTask.description.get()
+			poetryCmd.set(extension.poetryInitReadMeTask.poetryCmd)
+			projectName.set(extension.poetryInitReadMeTask.projectName)
+			projectDescription.set(extension.poetryInitReadMeTask.projectDescription)
+			readmeFile.set(extension.poetryInitReadMeTask.readmeFile)
+		}
+
+		project.tasks.register<PoetryInitPyProjectTask>("poetryInitPyProject") {
+			group = taskGroup
+			description = extension.poetryInitPyProjectTask.description.get()
+			poetryCmd.set(extension.poetryInitPyProjectTask.poetryCmd)
+			projectName.set(extension.poetryInitPyProjectTask.projectName)
+			projectDescription.set(extension.poetryInitPyProjectTask.projectDescription)
+			projectAuthor.set(extension.poetryInitPyProjectTask.projectAuthor)
+			projectHomepage.set(extension.poetryInitPyProjectTask.projectHomepage)
+			projectRepository.set(extension.poetryInitPyProjectTask.projectRepository)
+			readmeFile.set(extension.poetryInitPyProjectTask.readmeFile)
+			projectPythonVersion.set(extension.poetryInitPyProjectTask.projectPythonVersion)
+			mainSourcesDirectory.set(extension.poetryInitPyProjectTask.mainSourcesDirectory)
+			mainResourcesDirectory.set(extension.poetryInitPyProjectTask.mainResourcesDirectory)
+			testSourcesDirectory.set(extension.poetryInitPyProjectTask.testSourcesDirectory)
+			testResourcesDirectory.set(extension.poetryInitPyProjectTask.testResourcesDirectory)
+			pyprojectFile.set(extension.poetryInitPyProjectTask.pyprojectFile)
+
+			dependsOn(poetryInitProjectStructure, poetryInitReadMe)
+		}
+
+		/*
+		project.tasks.withType(GenerateIdeaModule::class.java).configureEach {
+
+			group = taskGroup
+			val moduleImlDirectoryPath = project.gradle.rootProject.rootProject.projectDir.toString() + "/.idea/modules" + project.projectDir.toString().replace(project.gradle.rootProject.rootProject.projectDir.toString(), "")
+			val moduleImlFileName = project.gradle.rootProject.name + project.projectDir.toString().replace(project.gradle.rootProject.rootProject.projectDir.toString(), "").replace("/", ".") + ".iml"
+
+			inputFile = File(moduleImlDirectoryPath, moduleImlFileName)
+			outputFile = File(moduleImlDirectoryPath, moduleImlFileName)
+			module.jdkName = "Poetry (${project.name})"
+
+			module.sourceDirs.add(File(project.projectDir, "src/main/python"))
+			module.resourceDirs.add(File(project.projectDir, "src/main/resources"))
+			module.testSources.from(File(project.projectDir, "src/test/python"))
+			module.testResources.from(File(project.projectDir, "src/test/resources"))
+
+		}
+		*/
 	}
 
+	/**
+	 * Setup global extension conventions.
+	 *
+	 * @param extension to set up
+	 */
+	private fun setupExtension(extension: PoetryExtension) {
+
+		// Set up poetryCmd convention
+		extension.poetryConfigInitTask.poetryCmd.convention(extension.poetryCmd)
+		extension.poetryInitProjectStructureTask.poetryCmd.convention(extension.poetryCmd)
+		extension.poetryInitReadMeTask.poetryCmd.convention(extension.poetryCmd)
+		extension.poetryInitPyProjectTask.poetryCmd.convention(extension.poetryCmd)
+
+		// Set up main sources directory convention
+		extension.poetryInitProjectStructureTask.mainSourcesDirectory.convention(extension.mainSourcesDirectory)
+		extension.poetryInitPyProjectTask.mainSourcesDirectory.convention(extension.mainSourcesDirectory)
+
+		// Set up main resources directory convention
+		extension.poetryInitProjectStructureTask.mainResourcesDirectory.convention(extension.mainResourcesDirectory)
+		extension.poetryInitPyProjectTask.mainResourcesDirectory.convention(extension.mainResourcesDirectory)
+
+		// Set up test sources directory convention
+		extension.poetryInitProjectStructureTask.testSourcesDirectory.convention(extension.testSourcesDirectory)
+		extension.poetryInitPyProjectTask.testSourcesDirectory.convention(extension.testSourcesDirectory)
+
+		// Set up test resources directory convention
+		extension.poetryInitProjectStructureTask.testResourcesDirectory.convention(extension.testResourcesDirectory)
+		extension.poetryInitPyProjectTask.testResourcesDirectory.convention(extension.testResourcesDirectory)
+
+		// Set up project name convention
+		extension.poetryInitReadMeTask.projectName.convention(extension.projectName)
+		extension.poetryInitPyProjectTask.projectName.convention(extension.projectName)
+
+		// Set up project description convention
+		extension.poetryInitReadMeTask.projectDescription.convention(extension.projectDescription)
+		extension.poetryInitPyProjectTask.projectDescription.convention(extension.projectDescription)
+
+		// Set up readme file convention
+		extension.poetryInitReadMeTask.readmeFile.convention(extension.readmeFile)
+		extension.poetryInitPyProjectTask.readmeFile.convention(extension.readmeFile)
+
+		// Set up project author convention
+		extension.poetryInitPyProjectTask.projectAuthor.convention(extension.projectAuthor)
+
+		// Set up project python version convention
+		extension.poetryInitPyProjectTask.projectPythonVersion.convention(extension.projectPythonVersion)
+
+	}
 }
--- poetry/src/main/kotlin/com/crow/gradle/plugins/poetry/PoetryPluginExtensions.kt
+++ poetry/src/main/kotlin/com/crow/gradle/plugins/poetry/PoetryPluginExtensions.kt
@@ -1,6 +1,291 @@
 package com.crow.gradle.plugins.poetry
 
+import javax.inject.Inject
+import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.OutputFile
+import org.gradle.kotlin.dsl.property
+
 /**
- * @author : error23
- * Created : 2/28/24
+ * Poetry plugin base extension. Used to configure poetry plugin tasks.
  */
+open class PoetryBaseExtension @Inject constructor(objects: ObjectFactory) {
+
+	/** Task description. */
+	val description = objects.property<String>()
+
+	/** Overrides global Poetry command to execute. */
+	val poetryCmd = objects.property<String>()
+
+}
+
+/**
+ * Poetry plugin config init task extension.
+ * Used to configure [com.crow.gradle.plugins.poetry.tasks.PoetryConfigInitTask] task.
+ */
+open class PoetryConfigInitTaskExtension @Inject constructor(objects: ObjectFactory, project: Project)
+	: PoetryBaseExtension(objects) {
+
+	/** Poetry configuration file. */
+	val poetryTomlFile = objects.fileProperty().convention(project.layout.projectDirectory.file("pyproject.toml"))
+
+	init {
+		description.convention("Initializes poetry configuration file.")
+		poetryCmd.convention("poetry")
+	}
+}
+
+/**
+ * Poetry plugin init project structure task extension.
+ * Used to configure [com.crow.gradle.plugins.poetry.tasks.PoetryInitProjectStructureTask] task.
+ */
+open class PoetryInitProjectStructureTaskExtension @Inject constructor(objects: ObjectFactory, project: Project)
+	: PoetryBaseExtension(objects) {
+
+	/**
+	 * Overrides global Directory containing main python sources.
+	 * This is the directory where your main application python code lives.
+	 */
+	val mainSourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/main/python"))
+
+	/**
+	 * Overrides global Directory containing main resources.
+	 * This is the directory where your main application resources live.
+	 */
+	val mainResourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/main/resources"))
+
+	/**
+	 * Overrides global Directory containing test python sources.
+	 * This is the directory where your test python code lives.
+	 */
+	val testSourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/test/python"))
+
+	/**
+	 * Overrides global Directory containing test resources.
+	 * This is the directory where your test resources live.
+	 */
+	val testResourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/test/resources"))
+
+	init {
+		description.convention("Initializes poetry project structure.")
+		poetryCmd.convention("poetry")
+	}
+}
+
+/**
+ * Poetry plugin readme task extension.
+ * Used to configure [com.crow.gradle.plugins.poetry.tasks.PoetryInitReadMeTask] task.
+ */
+open class PoetryInitReadMeTaskExtension @Inject constructor(objects: ObjectFactory, project: Project)
+	: PoetryBaseExtension(objects) {
+
+	/**
+	 * Overrides global project name.
+	 */
+	val projectName = objects.property<String>().convention(project.name)
+
+	/**
+	 * Overrides global project description.
+	 */
+	val projectDescription = objects.property<String>().convention(project.description?.removeSurrounding("'"))
+
+	/**
+	 * Overrides global readme file.
+	 */
+	val readmeFile = objects.fileProperty().convention(project.layout.projectDirectory.file("README.md"))
+
+	init {
+		description.convention("Generates README.md file.")
+		poetryCmd.convention("poetry")
+	}
+}
+
+/**
+ * Poetry plugin init pyproject task extension.
+ * Used to configure [com.crow.gradle.plugins.poetry.tasks.PoetryInitPyProjectTask] task.
+ */
+open class PoetryInitPyProjectTaskExtension @Inject constructor(objects: ObjectFactory, project: Project)
+	: PoetryBaseExtension(objects) {
+
+	/**
+	 * Overrides global project name.
+	 */
+	val projectName = objects.property<String>().convention(project.name)
+
+	/**
+	 * Overrides global project description.
+	 */
+	val projectDescription = objects.property<String>().convention(project.description?.removeSurrounding("'"))
+
+	/**
+	 * Overrides global project author.
+	 */
+	val projectAuthor = objects.property<String>().convention(project.findProperty("developerName").toString().plus(" <" + project.findProperty("developerEmail").toString() + ">"))
+
+	/**
+	 * Project homepage.
+	 */
+	val projectHomepage = objects.property<String>().convention(project.findProperty("projectUrl")?.toString())
+
+	/**
+	 * Project repository.
+	 */
+	val projectRepository = objects.property<String>().convention(project.findProperty("projectUrl")?.toString())
+
+	/**
+	 * Overrides global readme file.
+	 */
+	@get:InputFile
+	val readmeFile = objects.fileProperty().convention(project.layout.projectDirectory.file("README.md"))
+
+	/**
+	 * Overrides global python project version.
+	 */
+	val projectPythonVersion = objects.property<String>()
+
+	/**
+	 * Overrides global Directory containing main python sources.
+	 */
+	val mainSourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/main/python"))
+
+	/**
+	 * Overrides global Directory containing main resources.
+	 */
+	val mainResourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/main/resources"))
+
+	/**
+	 * Overrides global Directory containing test python sources.
+	 */
+	val testSourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/test/python"))
+
+	/**
+	 * Overrides global Directory containing test resources.
+	 */
+	val testResourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/test/resources"))
+
+	/**
+	 * Output pyproject.toml file.
+	 */
+	@get:OutputFile
+	val pyprojectFile = objects.fileProperty().convention(project.layout.projectDirectory.file("pyproject.toml"))
+
+	init {
+		description.convention("Initializes pyproject.toml file.")
+		poetryCmd.convention("poetry")
+	}
+}
+
+/**
+ * Poetry plugin extension.
+ * Used to configure poetry plugin tasks.
+ */
+@Suppress("Unused")
+abstract class PoetryExtension @Inject constructor(objects: ObjectFactory, project: Project) {
+
+	/**
+	 * Global Poetry command to execute.
+	 * @see [PoetryBaseExtension.poetryCmd]
+	 */
+	val poetryCmd = objects.property<String>().convention("poetry")
+
+	/**
+	 * Global Directory containing main python sources.
+	 * @see [PoetryInitProjectStructureTaskExtension.mainSourcesDirectory] and [PoetryInitPyProjectTaskExtension.mainSourcesDirectory]
+	 */
+	val mainSourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/main/python"))
+
+	/**
+	 * Global Directory containing main resources.
+	 * @see [PoetryInitProjectStructureTaskExtension.mainResourcesDirectory] and [PoetryInitPyProjectTaskExtension.mainResourcesDirectory]
+	 */
+	val mainResourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/main/resources"))
+
+	/**
+	 * Global Directory containing test python sources.
+	 * @see [PoetryInitProjectStructureTaskExtension.testSourcesDirectory] and [PoetryInitPyProjectTaskExtension.testSourcesDirectory]
+	 */
+	val testSourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/test/python"))
+
+	/**
+	 * Global Directory containing test resources.
+	 * @see [PoetryInitProjectStructureTaskExtension.testResourcesDirectory] and [PoetryInitPyProjectTaskExtension.testResourcesDirectory]
+	 */
+	val testResourcesDirectory = objects.directoryProperty().convention(project.layout.projectDirectory.dir("src/test/resources"))
+
+	/**
+	 * Global project name.
+	 * @see [PoetryInitReadMeTaskExtension.projectName] and [PoetryInitPyProjectTaskExtension.projectName]
+	 */
+	val projectName = objects.property<String>().convention(project.name)
+
+	/**
+	 * Global project description.
+	 * @see [PoetryInitReadMeTaskExtension.projectDescription] and [PoetryInitPyProjectTaskExtension.projectDescription]
+	 */
+	val projectDescription = objects.property<String>().convention(project.description?.removeSurrounding("'"))
+
+	/**
+	 *  Global readme file.
+	 *  @see [PoetryInitReadMeTaskExtension.readmeFile] and [PoetryInitPyProjectTaskExtension.readmeFile]
+	 */
+	val readmeFile = objects.fileProperty().convention(project.layout.projectDirectory.file("README.md"))
+
+	/**
+	 * Global project author.
+	 * @see [PoetryInitPyProjectTaskExtension.projectAuthor]
+	 */
+	val projectAuthor = objects.property<String>().convention(project.findProperty("developerName").toString().plus(" <" + project.findProperty("developerEmail").toString() + ">"))
+
+	/**
+	 * Global python project version.
+	 * @see [PoetryInitPyProjectTaskExtension.projectPythonVersion]
+	 */
+	val projectPythonVersion = objects.property<String>()
+
+	/**
+	 * [com.crow.gradle.plugins.poetry.tasks.PoetryConfigInitTask]
+	 * specific configuration.
+	 */
+	@get:Nested
+	abstract val poetryConfigInitTask: PoetryConfigInitTaskExtension
+
+	/**
+	 * [com.crow.gradle.plugins.poetry.tasks.PoetryInitProjectStructureTask]
+	 * specific configuration.
+	 */
+	@get:Nested
+	abstract val poetryInitProjectStructureTask: PoetryInitProjectStructureTaskExtension
+
+	/**
+	 * [com.crow.gradle.plugins.poetry.tasks.PoetryInitReadMeTask]
+	 * specific configuration.
+	 */
+	@get:Nested
+	abstract val poetryInitReadMeTask: PoetryInitReadMeTaskExtension
+
+	/**
+	 * [com.crow.gradle.plugins.poetry.tasks.PoetryInitPyProjectTask]
+	 * specific configuration.
+	 */
+	@get:Nested
+	abstract val poetryInitPyProjectTask: PoetryInitPyProjectTaskExtension
+
+	fun poetryConfigInitTask(action: Action<in PoetryConfigInitTaskExtension>) {
+		action.execute(poetryConfigInitTask)
+	}
+
+	fun poetryInitProjectStructureTask(action: Action<in PoetryInitProjectStructureTaskExtension>) {
+		action.execute(poetryInitProjectStructureTask)
+	}
+
+	fun poetryInitReadMeTask(action: Action<in PoetryInitReadMeTaskExtension>) {
+		action.execute(poetryInitReadMeTask)
+	}
+
+	fun poetryInitPyProjectTask(action: Action<in PoetryInitPyProjectTaskExtension>) {
+		action.execute(poetryInitPyProjectTask)
+	}
+}
